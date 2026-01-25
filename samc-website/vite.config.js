e5dfceb014fb05c3d76cb1f27a3f6289c88585a2
alsdp <alsdpxtv@gmail.com>
2026-01-25 10:19:24 +0100

    Change base path in Vite configuration

--- samc-website/vite.config.js
+++ samc-website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/S.A.M.C/', // AJOUTE CETTE LIGNE (Mets exactement le nom de ton dépôt GitHub)
+  base: '/', // AJOUTE CETTE LIGNE (Mets exactement le nom de ton dépôt GitHub)
 })
