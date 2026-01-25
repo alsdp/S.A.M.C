b927621bd5d8d4870ab86ba677ba173ba185debe
alsdp <alsdpxtv@gmail.com>
2026-01-25 09:56:52 +0100

    Add base URL to Vite configuration
    Set base URL for the project to the GitHub repository.

--- samc-website/vite.config.js
+++ samc-website/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/alsdp/S.A.M.C', // AJOUTE CETTE LIGNE (Mets exactement le nom de ton dépôt GitHub)
 })
